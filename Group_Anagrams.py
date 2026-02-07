3a7cc7c812c6ad4a2bff824e71a6da8e5e0f8469
boanergaes <mebitzeamanuel@gmail.com>
2026-02-07 21:07:32 +0300

    Implement function to group anagrams

---
+++ Group_Anagrams.py
@@ -0,0 +1,13 @@
+#leetcode
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        counts = {}
+        for s in strs:
+            count =  frozenset(Counter(s).items())
+            if count in counts:
+                counts[count].append(s)
+            else:
+                counts[count] = [s]
+
+        return list(counts.values())
