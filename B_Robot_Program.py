eca331d0277f6b5d645013e8685662e6aa043164
Zeamanuel Mebit <mebitzeamanuel@gmail.com>
2026-02-13 14:38:53 +0300

    B. Robot Program -- not solved yet

---
+++ B_Robot_Program.py
@@ -0,0 +1,22 @@
+t = int(input())
+
+for _ in range(t):
+    n, x, k = map(int, input().split())
+    cmd = input()
+    zero_count = 0
+    second = 0
+    
+    i = 0
+    while i < len(cmd) and second < k:
+        if cmd[i] == 'L':
+            x -= 1
+        elif cmd[i] == 'R':
+            x += 1
+        i += 1
+        if x == 0:
+            i = 0
+            zero_count += 1
+        
+        second += 1
+    
+    print(zero_count)
