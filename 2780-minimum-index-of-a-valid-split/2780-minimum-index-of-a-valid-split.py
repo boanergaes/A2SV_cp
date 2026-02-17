016f75512c5e2405923430b316fa0feefbc33943
boanergaes <mebitzeamanuel@gmail.com>
2026-02-17 11:59:44 +0300

    Time: 178 ms (5.62%), Space: 36.6 MB (6.18%) - LeetHub

--- 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.py
+++ 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.py
@@ -2,12 +2,6 @@ class Solution:
     def minimumIndex(self, nums: List[int]) -> int:
         n = len(nums)
         dominant = Counter(nums).most_common(1)[0][0]
-        # max_freq = 0
-        # count = Counter(nums)
-        # for k, val in count.items():
-        #     if val > max_freq:
-        #         max_freq = val
-        #         dominant = k
         i = 0
         l_count = Counter(nums[:i+1])
         r_count = Counter(nums[i+1:])
@@ -16,7 +10,6 @@ class Solution:
             return l_count.get(dominant, 0)*2 > i+1 and r_count.get(dominant, 0)*2 > n - i - 1
 
         while i < n-1:
-            # print(i, dominant, l_count, r_count)
             if isvalid(i, l_count, r_count):
                 return i
             i += 1
