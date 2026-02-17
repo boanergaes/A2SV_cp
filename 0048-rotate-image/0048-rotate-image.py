96e41e785ead2ea1628eea15f37a7b4127b9a5e8
boanergaes <mebitzeamanuel@gmail.com>
2026-02-17 21:39:36 +0300

    Time: 0 ms (100%), Space: 19.4 MB (49.44%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -10,10 +10,15 @@ class Solution:
             b = n - t - 1
 
             for i in range(r-l):
+                # hold top left value
                 temp = mat[t+i][l]
+                # move bottom left to top left
                 mat[t+i][l] = mat[b][l+i]
+                # move bottom right to bottom left 
                 mat[b][l+i] = mat[b-i][r]
+                # move top right to bottom bottom right
                 mat[b-i][r] = mat[t][r-i]
+                # move the held top left to top right
                 mat[t][r-i] = temp
             l += 1
             r -= 1
