99e77581fe601807318a25ab4207fe69bc23f01b
boanergaes <mebitzeamanuel@gmail.com>
2026-02-19 19:10:12 +0300

    Time: 785 ms (5.85%), Space: 21.5 MB (18.71%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.py
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.py
@@ -1,15 +1,22 @@
 class Solution:
     def smallestPalindrome(self, s: str) -> str:
         n = len(s)
-        if n == 1:
-            return s
+        s_count = sorted(s)
+        s_count = Counter(s_count)
+        answer = [""] * n
+        start, end = 0, n - 1
 
-        s = list(s)
-        srtd = sorted(s[:n//2])
+        for key, val in s_count.items():
+            pairs = val // 2
+            for _ in range(pairs):
+                answer[start] = key
+                answer[end] = key
+                start += 1
+                end -= 1
+
+        mid_char = s[n // 2]
 
         if n%2 != 0:
-            srtd.append(s[n//2])
+            answer[len(s)//2] = mid_char
 
-        srtd.extend(srtd[n//2 - 1::-1])
-
-        return ''.join(srtd)
+        return "".join(answer)
