5ad5877c546da2473a39f813750089ace9345b44
boanergaes <mebitzeamanuel@gmail.com>
2026-03-02 16:53:51 +0300

    Time: 69 ms (29.43%), Space: 43 MB (43.06%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.py
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.py
@@ -1,24 +1,24 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
         n = len(s)
-        arr = [0]*n
+        shift_magnitude = [0]*n
         for i in shifts:
             if i[2] == 1:
-                arr[i[0]] += 1
+                shift_magnitude[i[0]] += 1
                 if i[1] + 1 < n:
-                    arr[i[1] + 1] -= 1
+                    shift_magnitude[i[1] + 1] -= 1
             else:
-                arr[i[0]] -= 1
+                shift_magnitude[i[0]] -= 1
                 if i[1] + 1 < n:
-                    arr[i[1] + 1] += 1
+                    shift_magnitude[i[1] + 1] += 1
         
         for i in range(1, n):
-            arr[i] += arr[i-1]
+            shift_magnitude[i] += shift_magnitude[i-1]
 
         chars = []
         for i in range(n):
-            ch = ord(s[i]) - ord('a') + arr[i]
-            shifted = ch % 26 + ord('a')
+            ch = ord(s[i]) - ord('a')
+            shifted = (ch + shift_magnitude[i]) % 26 + ord('a')
             chars.append(chr(shifted))
 
         return ''.join(chars)
