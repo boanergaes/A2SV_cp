6a60598a104df263796b87d7167385f9a2f1b23a
boanergaes <mebitzeamanuel@gmail.com>
2026-03-04 16:53:41 +0300

    Time: 38 ms (18.95%), Space: 26.7 MB (34.38%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -6,8 +6,7 @@ class Solution:
 
         for i in range(1, n):
             prefix_sum.append(prefix_sum[-1] * nums[i-1])
+            si = n - i - 1
+            suffix_sum[si] = suffix_sum[si+1] * nums[si+1]
         
-        for i in range(n-2, -1, -1):
-            suffix_sum[i] = suffix_sum[i+1] * nums[i+1]
-
         return [prefix_sum[i] * suffix_sum[i] for i in range(n)]
