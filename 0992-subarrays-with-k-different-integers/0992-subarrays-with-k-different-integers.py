a71005ff42263a073b07c2ff8ce01f00087359c6
boanergaes <mebitzeamanuel@gmail.com>
2026-03-06 20:20:20 +0300

    Time: 149 ms (25.27%), Space: 22.1 MB (63.67%) - LeetHub

---
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.py
@@ -0,0 +1,30 @@
+class Solution:
+    def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
+        def dec_wind(wind, key):
+            wind[key] -= 1
+            if not wind[key]:
+                del wind[key]
+
+        n = len(nums)
+        window = {}
+        good = 0
+
+        far_left = 0
+        near_left = 0
+        for right in range(n):
+            window[nums[right]] = window.get(nums[right], 0) + 1
+
+            while len(window) > k:
+                dec_wind(window, nums[near_left])
+                near_left += 1
+                far_left = near_left
+            
+            while (window[nums[near_left]] > 1 and window[nums[right]] > 1) or window[nums[near_left]] > 1:
+                dec_wind(window, nums[near_left])
+                near_left += 1
+            
+            if len(window) == k:
+                print(near_left - far_left + 1)
+                good += near_left - far_left + 1
+
+        return good
