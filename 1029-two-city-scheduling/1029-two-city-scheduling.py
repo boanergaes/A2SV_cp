260f6b813b9d176f9d6a9799086906ddc5178444
boanergaes <mebitzeamanuel@gmail.com>
2026-03-13 18:10:38 +0300

    Time: 0 ms (100%), Space: 19.2 MB (92.46%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -1,22 +1,13 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
         n = len(costs)
-        costs.sort(key=lambda x : abs(x[0] - x[1]), reverse=True)
-        cost = a = b = 0
+        costs.sort(key=lambda x : x[0] - x[1])
+        cost = 0
 
         for i in range(n):
-            if a < n//2 and b < n//2:
-                if costs[i][0] < costs[i][1]:
-                    cost += costs[i][0]
-                    a += 1
-                else:
-                    cost += costs[i][1]
-                    b += 1
-            elif a < n//2:
+            if i < n//2:
                 cost += costs[i][0]
-                a += 1
-            elif b < n//2:
+            else:
                 cost += costs[i][1]
-                b += 1
-        
+
         return cost
