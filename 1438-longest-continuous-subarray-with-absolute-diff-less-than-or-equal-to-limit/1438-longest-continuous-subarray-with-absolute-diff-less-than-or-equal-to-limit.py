ff125e4a154751b593e8aa0fe28ee778442e3953
boanergaes <mebitzeamanuel@gmail.com>
2026-03-18 01:48:04 +0300

    Time: 179 ms (82.1%), Space: 30.1 MB (81.6%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,26 +1,25 @@
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
-        n = len(nums)
         minq = deque()
         maxq = deque()
-        ans = 0
-
+        maxlen = 0
         i = 0
-        for j in range(n):
-            while minq and nums[j] < minq[-1]:
+        for j in range(len(nums)):
+            while minq and  minq[-1] > nums[j]:
                 minq.pop()
             minq.append(nums[j])
-            while maxq and nums[j] > maxq[-1]:
+
+            while maxq and maxq[-1] < nums[j]:
                 maxq.pop()
             maxq.append(nums[j])
 
-            while maxq and minq and maxq[0] - minq[0] > limit:
+            while maxq[0] - minq[0] > limit:
                 if nums[i] == maxq[0]:
                     maxq.popleft()
                 if nums[i] == minq[0]:
                     minq.popleft()
                 i += 1
 
-            ans = max(ans, j - i + 1)
+            maxlen = max(maxlen, j - i + 1)
 
-        return ans 
+        return maxlen
