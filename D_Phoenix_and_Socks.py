5c8c1385d8d89ae61309ce2b1f19a22dafd3f5dd
Zeamanuel Mebit <mebitzeamanuel@gmail.com>
2026-03-18 01:57:29 +0300

    B. most socially distanced subsequence and D. phoenix and socks -- not solved and not understood

---
+++ D_Phoenix_and_Socks.py
@@ -0,0 +1,22 @@
+# ========== I DON'T UNDERSTAND!!! ================
+
+from collections import Counter
+
+t = int(input())
+
+for _ in range(t):
+    n, l, r = map(int, input().split())
+    socks = list(map(int, input().split()))
+    left = socks[:n//2]
+    right = Counter(socks[n//2:])
+    costs = abs(n//2 - l)
+
+    for num in left:
+        if num not in right or right[num] == 0:
+            costs += 1
+            continue
+
+        right[num] -= 1
+        
+    print(costs)
+    
