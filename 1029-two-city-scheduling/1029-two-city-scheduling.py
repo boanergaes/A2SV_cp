f1ab474ce50c584cf7ab3cccf64150e4ab0c7380
boanergaes <mebitzeamanuel@gmail.com>
2026-03-19 02:55:12 +0300

    Time: 0 ms (100%), Space: 19.2 MB (86.2%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -3,11 +3,8 @@ class Solution:
         n = len(costs)
         costs.sort(key=lambda x : x[0] - x[1])
         cost = 0
-
-        for i in range(n):
-            if i < n//2:
-                cost += costs[i][0]
-            else:
-                cost += costs[i][1]
-
+        for i in range(n//2):
+            cost += costs[i][0]
+        for i in range(n//2, n):
+            cost += costs[i][1]
         return cost
