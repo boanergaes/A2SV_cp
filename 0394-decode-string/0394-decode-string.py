1819c3b67fd75b8e03cb30baa2c2ab3312aacf7d
boanergaes <mebitzeamanuel@gmail.com>
2026-03-18 23:16:30 +0300

    Time: 0 ms (100%), Space: 19.4 MB (33.42%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,28 +1,27 @@
 class Solution:
     def decodeString(self, s: str) -> str:
         def solve(i):
-            result = []
+            res = []
             num = 0
 
             while i < len(s):
                 c = s[i]
 
-                if c.isdigit():
+                if c.isnumeric():
                     num = num * 10 + int(c)
                     i += 1
-
+                
                 elif c == '[':
-                    inner, i = solve(i + 1)
-                    result.append(inner * num)
+                    inner, i = solve(i+1)
+                    res.append(inner * num)
                     num = 0
-
+                
                 elif c == ']':
-                    return ''.join(result), i + 1
-
-                else:  # letter
-                    result.append(c)
+                    return ''.join(res), i+1
+                else:
+                    res.append(c)
                     i += 1
-
-            return ''.join(result), i
+                    
+            return ''.join(res), i
 
         return solve(0)[0]
