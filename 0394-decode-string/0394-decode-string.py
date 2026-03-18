e83557e5ed0b07adb1af0ff0acbbc43d8df7c11e
boanergaes <mebitzeamanuel@gmail.com>
2026-03-18 22:48:55 +0300

    Time: 0 ms (100%), Space: 19.3 MB (68.95%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,53 +1,28 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        def solve(k, rem):
-            cbn = []
+        def solve(i):
+            result = []
             num = 0
-            i = 0
-            while i < len(rem):
-                c = rem[i]
-                if c.isnumeric():
-                    narr = []
-                    while rem[i].isnumeric():
-                        narr.append(rem[i])
-                        i += 1
-                    num = int(''.join(narr))
-                elif c.isalpha():
-                    cbn.append(c)
-                    i += 1
-                elif c == '[':
-                    incoming, curri = solve(num, rem[i+1:])
-                    cbn += incoming
-                    i += curri + 2
-                elif c == ']':
-                    final = []
-                    for _ in range(k):
-                        final.extend(cbn)
-                    return [final, i]
 
-        num = 0
-        decoded = []
-        char_buff = []
-        i = 0
-        while i < len(s):
-            c = s[i]
-            if c.isnumeric():
-                narr = []
-                while s[i].isnumeric():
-                    narr.append(s[i])
+            while i < len(s):
+                c = s[i]
+
+                if c.isdigit():
+                    num = num * 10 + int(c)
                     i += 1
-                num = int(''.join(narr))
-                decoded.extend(char_buff)
-                char_buff = []
-            elif c == '[':
-                incoming, curri = solve(num, s[i+1:])
-                decoded += incoming
-                decoded.extend(char_buff)
-                char_buff = []
-                i += curri + 2
-            elif c.isalpha():
-                char_buff.append(c)
-                i += 1
-        
-        decoded.extend(char_buff)
-        return ''.join(decoded)
+
+                elif c == '[':
+                    inner, i = solve(i + 1)
+                    result.append(inner * num)
+                    num = 0
+
+                elif c == ']':
+                    return ''.join(result), i + 1
+
+                else:  # letter
+                    result.append(c)
+                    i += 1
+
+            return ''.join(result), i
+
+        return solve(0)[0]
