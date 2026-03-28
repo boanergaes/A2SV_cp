4064bb7323632ac6ef899125b4f8a2c044c81be1
boanergaes <mebitzeamanuel@gmail.com>
2026-03-29 00:12:17 +0300

    Time: 4891 ms (13%), Space: 19.4 MB (56.87%) - LeetHub

---
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -0,0 +1,23 @@
+class Solution:
+    def distributeCookies(self, cookies: List[int], k: int) -> int:
+        n = len(cookies)
+        min_unfair = float('inf')
+        dist = [0]*k
+
+        def backtrack(i):
+            nonlocal min_unfair
+
+            if i == n:
+                min_unfair = min(min_unfair, max(dist))
+                return
+
+            if min_unfair <= max(dist): return
+            
+            for j in range(k):
+                dist[j] += cookies[i]
+                backtrack(i+1)
+                dist[j] -= cookies[i]
+
+        backtrack(0)
+
+        return min_unfair
