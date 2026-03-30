c3562638c3766628a57c855b089ca319cfa3705c
boanergaes <mebitzeamanuel@gmail.com>
2026-03-31 01:04:36 +0300

    Time: 2 ms (23.41%), Space: 19.5 MB (43.47%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        n = len(nums)
+        powerSet = []
+        path = []
+
+        def backtrack(i):
+            powerSet.append(path[:])
+            
+            for j in range(i, n):
+                if j > i and nums[j] == nums[j-1]:
+                    continue
+
+                path.append(nums[j])
+                backtrack(j+1)
+                path.pop()
+
+        backtrack(0)
+
+        return list(powerSet)
