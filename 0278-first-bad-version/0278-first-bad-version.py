441da1e3a8a53bdd7af18f2dcb3a864c32e6a5bf
boanergaes <mebitzeamanuel@gmail.com>
2026-04-02 18:10:40 +0300

    Time: 44 ms (57.91%), Space: 19.4 MB (16.21%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -9,11 +9,9 @@ class Solution:
         while low <= high:
             mid = (low + high) // 2
 
-            bad = isBadVersion(mid)
-
-            if not bad:
-                low = mid + 1
-            else:
+            if isBadVersion(mid):
                 high = mid - 1
-
+            else:
+                low = mid + 1
+        
         return low
