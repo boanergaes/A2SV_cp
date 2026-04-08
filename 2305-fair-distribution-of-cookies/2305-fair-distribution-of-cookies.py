3926c7c149f24e291fba8553c034d365605b5628
boanergaes <mebitzeamanuel@gmail.com>
2026-04-08 18:21:28 +0300

    Time: 1096 ms (31.5%), Space: 19.3 MB (50.49%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -10,15 +10,12 @@ class Solution:
             if i == n:
                 min_unfair = min(min_unfair, max(dist))
                 return
-
-            # if min_unfair <= max(dist): return
             
             for j in range(k):
                 if dist[j] + cookies[i] >= min_unfair:
                     continue
-
                 dist[j] += cookies[i]
-                backtrack(i+1)
+                backtrack(i + 1)
                 dist[j] -= cookies[i]
 
         backtrack(0)
