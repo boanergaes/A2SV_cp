31061630a5d37ea24649f0da81abc63a49545637
boanergaes <mebitzeamanuel@gmail.com>
2026-04-18 00:52:40 +0300

    Time: 479 ms (5.04%), Space: 24 MB (15.27%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -1,28 +1,24 @@
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
-        tot_weight = sum(weights)
-        def valid(cap):
-            curr = 0
-            tot = 0
+        n = len(weights)
+        tot = sum(weights)
+        def valid(capacity):
+            curr_tot = 0
             i = 0
             for _ in range(days):
-                while i < len(weights) and curr + weights[i] <= cap:
-                    curr += weights[i]
-                    i += 1 
-                tot += curr
                 curr = 0
-
-            return tot == tot_weight
+                while i < n and curr + weights[i] <= capacity:
+                    curr += weights[i]
+                    i += 1
+                curr_tot += curr
+            return curr_tot == tot
 
         low = max(weights)
-        high = tot_weight
-
+        high = tot
         while low <= high:
             mid = low + (high - low) // 2
-
             if valid(mid):
                 high = mid - 1
             else:
                 low = mid + 1
-
         return low
