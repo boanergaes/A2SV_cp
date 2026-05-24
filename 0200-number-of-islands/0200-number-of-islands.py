e9914ea99529c77d936133034879b01b7c21acbd
boanergaes <mebitzeamanuel@gmail.com>
2026-05-24 22:01:47 +0300

    Time: 259 ms (30.7%), Space: 31.2 MB (9.37%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,25 +1,27 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def inbound(r, c):
+        dir = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        vis = set()
+
+        def isbound(r, c):
             return 0 <= r < len(grid) and 0 <= c < len(grid[0])
 
-        dir = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        vis = set()
-        islands = 0
-
         def dfs(row, col):
-            vis.add((row, col))
+            nonlocal vis
 
-            for x, y in dir:
-                newr, newc = row + x, col + y
+            for r, c in dir:
+                newr = row + r
+                newc = col + c
 
-                if inbound(newr, newc) and grid[row][col] == '1' and (newr, newc) not in vis:
-                    dfs(newr, newc)   
+                if (newr, newc) not in vis and isbound(newr, newc) and grid[newr][newc] == '1':
+                    vis.add((newr, newc))
+                    dfs(newr, newc)
 
+        islands = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == '1' and (i, j) not in vis:
                     dfs(i, j)
                     islands += 1
 
-        return islands             
+        return islands
