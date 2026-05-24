7a91371ff198fc5105b851543f654f954d017729
boanergaes <mebitzeamanuel@gmail.com>
2026-05-25 01:31:51 +0300

    Time: 0 ms (100%), Space: 20.7 MB (12.44%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -2,12 +2,9 @@ class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
         # -1: unvisited  0: black  1: white
 
-        color_arr = [-1] * len(graph)
+        color_arr = [0] + [-1] * (len(graph) - 1)
 
         def dfs(curr):
-            if color_arr[curr] == -1:          
-                color_arr[curr] = 0
-
             for nei in graph[curr]:
                 if color_arr[nei] == color_arr[curr]:
                     return False
@@ -18,6 +15,8 @@ class Solution:
             return True
         
         for node in range(len(graph)):
+            if color_arr[node] == -1:
+                color_arr[node] = 0
             if not dfs(node):
                 return False
 
