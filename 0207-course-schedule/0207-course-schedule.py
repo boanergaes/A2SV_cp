898bcf8a0e8a4e7ccf4ef1efc2abdfca78cdd033
boanergaes <mebitzeamanuel@gmail.com>
2026-05-25 11:41:31 +0300

    Time: 4 ms (64.81%), Space: 21.2 MB (34.43%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,29 +1,30 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         # -1: gray(unvisited)  0: white(expanded)  1: black(finished)
-        color = [-1] * numCourses
+        GRAY, WHITE, BLACK = -1, 0, 1
+        color = [GRAY] * numCourses
         graph = defaultdict(list)
 
         for i, j in prerequisites:
             graph[i].append(j)
 
         def iscycle(node):
-            if color[node] == 0:
+            if color[node] == WHITE:
                 return False
 
-            color[node] = 0
+            color[node] = WHITE
 
             for nei in graph[node]:
-                if color[nei] != 1:
+                if color[nei] != BLACK:
                     if not iscycle(nei):
                         return False
 
-            color[node] = 1
+            color[node] = BLACK
             return True
 
 
         for node in range(numCourses):
-            if color[node] != 1 and not iscycle(node):
+            if color[node] != BLACK and not iscycle(node):
                 return False
 
         return True
